9124764581122ae03fc38ea716d5e79017f80376
Furia25 <valentinledur@gmail.com>
2026-05-16 02:32:57 +0200

    Cleaned duplicate functions

--- src/HTTP/Request.cpp
+++ src/HTTP/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: vdurand <vdurand@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 15:03:13 by antoine           #+#    #+#             */
-/*   Updated: 2026/05/16 02:32:09 by vdurand          ###   ########.fr       */
+/*   Updated: 2026/05/16 02:32:36 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,6 @@ Request::Request() : method(Method::GET),
 		is_chunked(false),
 		keep_alive(false)
 {}
-
-const std::string	*Request::operator[](const std::string &key) const
-{
-	Headers::const_iterator it = this->headers.find(key);
-	return it == this->headers.end() ? NULL : &it->second;
-}
-
-const std::string	*Request::operator[](const char *key) const
-{
-	Headers::const_iterator it = this->headers.find(key);
-	return it == this->headers.end() ? NULL : &it->second;
-}
-
-const std::string	*Request::operator[](const std::string &key) const
-{
-	Headers::const_iterator it = this->headers.find(key);
-	return it == this->headers.end() ? NULL : &it->second;
-}
-
-const std::string	*Request::operator[](const char *key) const
-{
-	Headers::const_iterator it = this->headers.find(key);
-	return it == this->headers.end() ? NULL : &it->second;
-}
 
 const std::string	*Request::operator[](const std::string &key) const
 {
